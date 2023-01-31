8c60185ed16e1f77cdfb8dd4c987f1954e110735
Hans Friedrich <4332467+hansfriedrich@users.noreply.github.com>
2023-01-31 22:12:39 +0100

    logging if last update is longer than 5 min

--- src/sensor-profiles/EEP_D2_14_41.ts
+++ src/sensor-profiles/EEP_D2_14_41.ts
@@ -8,60 +8,71 @@ import { EnoceanSensorHomebridgePlatform } from '../platform';
 export class EEP_D2_14_41 extends EEPProfile {
 
 
-  private temperature =0;
-  private humidity =0;
-  private illumination=0.001;
+  private temperature = 0;
+  private humidity = 0;
+  private illumination = 0.001;
+
+  private lastUpdate: number;
 
   constructor(platform: EnoceanSensorHomebridgePlatform, accesory: PlatformAccessory<EnoceanSensorContext>) {
     super(platform, accesory, [platform.Service.TemperatureSensor]);
 
     const temperatureSensorService = accesory.getService(platform.Service.TemperatureSensor)
-    || accesory.addService(platform.Service.TemperatureSensor);
+      || accesory.addService(platform.Service.TemperatureSensor);
     (temperatureSensorService.getCharacteristic(platform.Characteristic.CurrentTemperature) ||
-    temperatureSensorService.addCharacteristic(platform.Characteristic.CurrentTemperature))
+      temperatureSensorService.addCharacteristic(platform.Characteristic.CurrentTemperature))
       .onGet(() => this.temperature);
 
     const humidity_service = accesory.getService(platform.Service.HumiditySensor)
-    || accesory.addService(platform.Service.HumiditySensor);
+      || accesory.addService(platform.Service.HumiditySensor);
     (humidity_service.getCharacteristic(platform.Characteristic.CurrentRelativeHumidity) ||
-    humidity_service.addCharacteristic(platform.Characteristic.CurrentRelativeHumidity))
-      .onGet(()=>this.humidity);
+      humidity_service.addCharacteristic(platform.Characteristic.CurrentRelativeHumidity))
+      .onGet(() => this.humidity);
 
     const illumination_service = accesory.getService(platform.Service.LightSensor)
-    || accesory.addService(platform.Service.LightSensor);
+      || accesory.addService(platform.Service.LightSensor);
     (illumination_service.getCharacteristic(platform.Characteristic.CurrentAmbientLightLevel) ||
       illumination_service.addCharacteristic(platform.Characteristic.CurrentAmbientLightLevel))
-      .onGet(()=>this.illumination);
+      .onGet(() => this.illumination);
+
+    this.lastUpdate = Date.now();
+    setInterval(() => {
+      if (Date.now() - this.lastUpdate > 300000) {
+        this.platform.log.error('no updates from device', this.accessory.context.id,
+          'since ', (Date.now() - this.lastUpdate) / 60000, 'min');
+      }
+    }, 300000);
   }
 
 
 
 
   on_message(telegram: ERP1Telegram): void {
+    this.lastUpdate = Date.now();
     const userData = telegram.userData;
 
     const temp_raw = (userData.readUInt8(0) << 2) + (userData.readUInt8(1) >>> 6);
-    this.temperature = temp_raw/10 - 40;
-    this.platform.log.debug('temperature: ', temp_raw/10 - 40, ' °C');
+    this.temperature = temp_raw / 10 - 40;
+    this.platform.log.debug('temperature: ', temp_raw / 10 - 40, ' °C');
 
     const humidity_raw = ((userData.readUInt8(1) & 0x3F) << 2) + (userData.readUInt8(2) >>> 6);
     this.humidity = humidity_raw / 2;
-    this.platform.log.debug('humidity: ', humidity_raw/2, ' %');
+    this.platform.log.debug('humidity: ', humidity_raw / 2, ' %');
 
     const illumination_raw = ((userData.readUInt8(2) & 0x3F) << 11) + (userData.readUInt8(3) << 3) + (userData.readUInt8(4) >>> 5);
-    this.illumination = illumination_raw+0.001;
+    this.illumination = illumination_raw + 0.001;
     this.platform.log.debug('illumionation: ', illumination_raw, ' lx');
 
     const temp_service = this.accessory.getService(this.platform.Service.TemperatureSensor)
-    || this.accessory.addService(this.platform.Service.TemperatureSensor);
+      || this.accessory.addService(this.platform.Service.TemperatureSensor);
     temp_service.updateCharacteristic(this.platform.Characteristic.CurrentTemperature, this.temperature);
 
     const humidiity_service = this.accessory.getService(this.platform.Service.HumiditySensor)
-    || this.accessory.addService(this.platform.Service.HumiditySensor);
+      || this.accessory.addService(this.platform.Service.HumiditySensor);
     humidiity_service.updateCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity, this.humidity);
 
     const illumination_service = this.accessory.getService(this.platform.Service.LightSensor)
-    || this.accessory.addService(this.platform.Service.LightSensor);
+      || this.accessory.addService(this.platform.Service.LightSensor);
     illumination_service.updateCharacteristic(this.platform.Characteristic.CurrentAmbientLightLevel, this.illumination);
   }
 }
